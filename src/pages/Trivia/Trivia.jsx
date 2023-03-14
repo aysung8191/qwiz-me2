7a3d3a96d8d842b08acb05736a7dda40dcac9674
aysung8191 <aysung8191@gmail.com>
2023-03-14 11:34:35 -0700

    adds category and number of questions functionality for Trivia section

--- src/pages/Trivia/Trivia.jsx
+++ src/pages/Trivia/Trivia.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import Flashcard from '../../components/Flashcard/Flashcard'
-import {useEffect} from 'react'
+import {useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 
 export default function Trivia({ flashcards, setFlashcards }) {
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api_category.php')
       .then(res => {
-        setFlashcards(res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer)
-          const options = [
-            ...questionItem.incorrect_answers.map(a => decodeString(a)), 
-            answer
-          ]
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: questionItem.correct_answer,
-            options: options.sort(() => Math.random() - .5)
-          }
-        }))
+        setCategories(res.data.trivia_categories)
       })
+  }, [])
+
+  useEffect(() => {
   }, [])
 
   function decodeString(str) {
@@ -31,11 +26,57 @@ export default function Trivia({ flashcards, setFlashcards }) {
     return textArea.value
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+  }
+
   return (
-    <div className="card-grid">
-        {flashcards.map(flashcard => {
-            return <Flashcard flashcard={flashcard} key={flashcard.id} />
-        })}
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of Questions</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={20} ref={amountEl} />
+        </div>
+        <div className="form-group">
+            <button className="btn">Generate Questions</button>
+        </div>
+      </form>
+      <br />
+      <div className="card-grid">
+          {flashcards.map(flashcard => {
+              return <Flashcard flashcard={flashcard} key={flashcard.id} />
+          })}
+      </div>
+    </>
   )
 }
